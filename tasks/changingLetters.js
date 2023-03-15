9c765d8e2a72b879f75b01e0509a92fc7dd3690f
YungShadi <exlatedorb@gmail.com>
2023-03-15 12:14:45 +0300

    letters

---
+++ tasks/changingLetters.js
@@ -0,0 +1,12 @@
+function swap (string) {
+    let result = ``;
+    for (i = 0; i< string.length; i++){
+        if(string[i] === `a` || string[i] === `e` || string[i] === `i` || string[i] === `o` || string[i] === `u` ){
+           result += string[i].toUpperCase();
+        
+        }else{
+            result += string[i]
+        }
+    }return result
+}
+console.log(swap(`Hello World`))
