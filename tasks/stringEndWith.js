5b02765ea47a37a064120bd64d614e8ee34246cc
YungShadi <exlatedorb@gmail.com>
2023-03-23 21:04:22 +0300

    susy

---
+++ tasks/stringEndWith.js
@@ -0,0 +1,13 @@
+let lengthOfLastWord = function(s) {
+    s = s.split(` `);
+    let lastWord = ``;
+    for (i = 1; i <= s.length; i++){
+        if ( s[s.length - i] !== ``){
+            lastWord = s[s.length - i]
+            break;
+        }
+    }
+    let lastLength = lastWord.length;
+    return lastLength;
+};
+console.log(lengthOfLastWord(`Bebrochka med sus  `))
